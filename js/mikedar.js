a89e6dcb75e2ff93e6769a7917402a80165d14d8
erikreed <ereed.uw@gmail.com>
2012-07-07 06:42:52 -0400

    some blip action. and bugs.

--- js/mikedar.js
+++ js/mikedar.js
@@ -1,12 +1,14 @@
 // mikedar.js
 
-var frame_ = 0;
-var canvas; 
-var context;
-var centerX;
-var centerY;
-var radius = 70;
-
+// constants
+var speed = (5 * Math.PI); // higher = faster radar
+var numActive = 3;
+var blipDuration = 100;
+var blipX = 15;
+var blipY = 10;
+var blipAlpha = .8;
+var blipLives = 3;
+var refreshRate = 30; // ms
 // lazy gradient hack
 var rS = 53;
 var gS = 150;
@@ -14,6 +16,19 @@ var bS = 6;
 var rD = 85 - rS;
 var gD = 252 - gS;
 var bD = 7 - bS;
+
+// vars
+var frame_ = 0;
+var canvas; 
+var context;
+var centerX;
+var centerY;
+var radius = 70;
+
+// map of {x, y, t}
+var blips = [];
+
+
 
 		
 window.onload = function() {
@@ -24,13 +39,17 @@ window.onload = function() {
 
 		setInterval(function() {
 			draw(frame_++);
-		},50);
-
+		},refreshRate);
+		
+		for (var i=0; i<numActive; i++) {
+			blips.push(getBlip());
+		}
       };
 
 function draw() {
-	var frame = frame_ / (.5 * Math.PI);
-	frame /= 10;
+	context.clearRect(0,0,600,400);
+	var frame = frame_ / speed;
+	frame %= 2 * Math.PI;
 
 	context.globalAlpha = 1;
 	// background circle
@@ -60,4 +79,29 @@ function draw() {
 		context.fillStyle = rgb;
 		context.fill();
 	}
+	
+	for (var i=0; i<numActive; i++) {
+		var blip = blips[i];
+		if (--blip.t < 0) {
+			blip.t = blipDuration;
+			if (--blip.lives <= 0) {
+				blip = getBlip();
+			}
+			blips[i] = blip;
+		}
+		context.globalAlpha = blipAlpha * blip.t / blipDuration;
+		context.beginPath();
+        context.rect(centerX + blip.x, centerY + blip.y, blipX, blipY);
+        context.fillStyle = '#6AC934';
+        context.fill();
+	}
+}
+
+function getBlip() {
+	var blip = [];
+	blip.x = (Math.random() - .5) * radius * 2;
+	blip.y = (Math.random() - .5) * radius * 2;
+	blip.t = blipDuration;
+	blip.lives = blipLives;
+	return blip;
 }
